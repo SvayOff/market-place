38dc95c247a7ddbbf938ef9b0d04b191952b9a5b
SvayOff <marsiul2001@gmail.com>
2021-02-24 15:23:01 +0200

    added slider and featured and release

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,7 @@
 $(function () {
-
+    $('.featured-slider__inner').slick({
+        prevArrow: '<button class="slick-arrow slick-prev lnr lnr-chevron-left"></button>',
+        nextArrow: '<button class="slick-arrow slick-next lnr lnr-chevron-right"></button>',
+    });
 
 });
