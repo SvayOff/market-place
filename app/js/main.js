ab7769af458d3ea650311a8c628f8afef3b15036
SvayOff <marsiul2001@gmail.com>
2021-02-25 19:09:51 +0200

    added feed and total

--- app/js/main.js
+++ app/js/main.js
@@ -3,5 +3,11 @@ $(function () {
         prevArrow: '<button class="slick-arrow slick-prev lnr lnr-chevron-left"></button>',
         nextArrow: '<button class="slick-arrow slick-next lnr lnr-chevron-right"></button>',
     });
+    $('.feed-slider__inner').slick({
+        prevArrow: '<button class="slick-arrow slick-prev lnr lnr-chevron-left"></button>',
+        nextArrow: '<button class="slick-arrow slick-next lnr lnr-chevron-right"></button>',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+    });
 
 });
