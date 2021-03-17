b3ab3d23f4015df9d9585e17e78326b8d0774285
SvayOff <marsiul2001@gmail.com>
2021-03-17 22:51:11 +0200

    added blog.html

--- app/js/main.js
+++ app/js/main.js
@@ -10,6 +10,15 @@ $(function () {
         slidesToScroll: 1,
     });
 
+    $('.posts__tabs .tab').on('click', function (event) {
+        var id = $(this).attr('data-id');
+        $('.posts__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.posts__tabs .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#' + id).addClass('active-tab').fadeIn();
+        return false;
+    });
+
     $('input[type="checkbox"]').styler();
 
 });
